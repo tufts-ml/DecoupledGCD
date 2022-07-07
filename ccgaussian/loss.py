2c7c1d879b26a56ea84652f86dce1aaf00e1606b
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-07-07 15:24:57 -0400

    NDCC Loss

---
+++ ccgaussian/loss.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn
+import torch.nn.functional as f
+
+
+class NDCCLoss(torch.nn.Module):
+    def __init__(self, w_ccg, w_nll) -> None:
+        super().__init__()
+        self.w_ccg = w_ccg
+        self.w_nll = w_nll
+
+    @staticmethod
+    def ce_loss(logits, targets):
+        return f.cross_entropy(logits, targets)
+
+    @staticmethod
+    def mahalanobis_d(norm_embeds, means, sigma2s, targets):
+        return ((norm_embeds - means[targets])**2 / sigma2s).sum() / \
+            (2 * norm_embeds.shape[0])
+
+    @staticmethod
+    def md_loss(norm_embeds, means, sigma2s, targets):
+        # Mahalanobis distance loss, affects embedding and means
+        return NDCCLoss.mahalanobis_d(norm_embeds, means, sigma2s.detach(), targets)
+
+    @staticmethod
+    def nll_loss(norm_embeds, means, sigma2s, targets):
+        # negative log-likelihood loss, affects means and covaraiance
+        return torch.log(sigma2s).sum() / 2 + \
+            NDCCLoss.mahalanobis_d(norm_embeds.detach(), means, sigma2s, targets)
+
+    def forward(self, logits, norm_embeds, means, sigma2s, targets):
+        return self.ce_loss(logits, targets) + self.w_ccg * (
+            self.md_loss(norm_embeds, means, sigma2s, targets) +
+            self.w_nll * self.nll_loss(norm_embeds, means, sigma2s, targets)
+        )
