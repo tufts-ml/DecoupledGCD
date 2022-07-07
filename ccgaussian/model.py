81ff898be498ab91bddb67a7144b7a9b59cf05be
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-07-07 15:25:55 -0400

    Variance Lower Bound

--- ccgaussian/model.py
+++ ccgaussian/model.py
@@ -15,7 +15,15 @@ class DinoCCG(nn.Module):
         self.classifier = nn.Linear(self.embed_len, num_classes)
         # class-conditional Gaussian parameters
         self.sigma = torch.ones((1,), dtype=torch.float32, requires_grad=True)
-        self.delta = torch.zeros((self.embed_len,), dtype=torch.float32, requires_grad=True)
+        self.deltas = torch.zeros((self.embed_len,), dtype=torch.float32, requires_grad=True)
+        # variance lower bound to prevent very large losses
+        self.var_min = .1
+
+    def gaussian_params(self):
+        # class-conditional Gaussian parameters
+        sigma2s = (self.sigma + self.deltas)**2 + self.var_min
+        means = self.classifier.weight * sigma2s
+        return means, sigma2s
 
     def forward(self, x):
         # normalized DINO embeddings
@@ -23,7 +31,5 @@ class DinoCCG(nn.Module):
         norm_embeds = self.embed_mag * normalize(embeds.view(embeds.shape[0], -1), dim=1)
         # classifier prediction
         logits = self.classifier(norm_embeds)
-        # class-conditional Gaussian parameters
-        sigma2s = (self.sigma + self.delta) ** 2
-        means = self.classifier.weight * sigma2s
+        means, sigma2s = self.gaussian_params()
         return logits, norm_embeds, means, sigma2s
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,9 @@
 import pytest
 import torch
+from torch.optim import SGD
 
-from ccgaussian.models import DinoCCG
+from ccgaussian.loss import NDCCLoss
+from ccgaussian.model import DinoCCG
 
 
 input_shape = (8, 3, 32, 32)
@@ -19,3 +21,28 @@ class TestDinoCCG():
         assert norm_embeds.shape == (input_shape[0], model.embed_len)
         assert means.shape == (num_classes, model.embed_len)
         assert sigma2s.shape == (model.embed_len,)
+
+    def test_var_stability(self, model):
+        norm_embeds = torch.zeros((input_shape[0], model.embed_len))
+        norm_embeds[:, :4] = 4
+        targets = torch.zeros((input_shape[0],), dtype=int)
+        optimizer = SGD([
+            {"params": [model.sigma, model.deltas], "lr": 1e-4, "momentum": .9},
+            {"params": [model.classifier.weight], "lr": 1e-1, "momentum": .9}])
+        # generous loss threshold that should be easy to reach and stay under
+        loss_thresh = -400
+        passed_loss_thresh = False
+        for _ in range(100):
+            optimizer.zero_grad()
+            means, sigma2s = model.gaussian_params()
+            loss_nll = NDCCLoss.nll_loss(norm_embeds, means, sigma2s, targets)
+            loss_md = NDCCLoss.md_loss(norm_embeds, means, sigma2s, targets)
+            loss = loss_nll + loss_md
+            loss.backward()
+            optimizer.step()
+            if passed_loss_thresh and loss > loss_thresh:
+                pytest.fail("Optimization not stable, went under then above threshold")
+            if loss < loss_thresh:
+                passed_loss_thresh = True
+        if passed_loss_thresh is False:
+            pytest.fail("Optimization failed to reach threshold")
