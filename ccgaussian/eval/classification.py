cff0e2e969de11a4e882f52ba1f495d4c1ce2bcc
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-07-28 15:43:55 -0400

    Basic Classification Eval

---
+++ ccgaussian/eval/classification.py
@@ -0,0 +1,46 @@
+import torch
+
+from polycraft_nov_data.dataloader import novelcraft_dataloader
+
+from ccgaussian.dino_trans import DINOTestTrans
+from ccgaussian.load import load_dino_ccg
+
+
+def sq_mahalanobis_dist(embeds, means, sigma2s):
+    """Squared Mahalanobis distance
+
+    Args:
+        embeds (torch.Tensor): Embeddings (N, D)
+        means (torch.Tensor): Means (K, D)
+        sigma2s (torch.Tensor): Diagonal of covariance matrix (D,)
+
+    Returns:
+        torch.Tensor: Distances between embedding n and mean k, (N, K)
+    """
+    # N x K x D
+    difs = torch.unsqueeze(embeds, 1) - torch.unsqueeze(means, 0)
+    # D x D
+    inv_cov = torch.diag_embed(1 / sigma2s)
+    # N x K x 1 x D @ D x D @ N x K x D x 1 = N x K x 1 x 1
+    dists = torch.unsqueeze(difs, 2) @ inv_cov @ torch.unsqueeze(difs, 3)
+    # N x K
+    return torch.squeeze(dists)
+
+
+if __name__ == "__main__":
+    device = torch.device("cuda:0")
+    model = load_dino_ccg(device)
+    valid_dataloader = novelcraft_dataloader("valid_norm", DINOTestTrans(), batch_size=128)
+    classifier_pred = torch.tensor([])
+    ccg_pred = torch.tensor([])
+    true_targets = torch.tensor([])
+    for data, targets in valid_dataloader:
+        logits, norm_embeds, means, sigma2s = model(data.to(device))
+        logits = logits.to("cpu")
+        dists = sq_mahalanobis_dist(norm_embeds, means, sigma2s).to("cpu")
+        classifier_pred = torch.hstack((classifier_pred, torch.argmax(logits, 1)))
+        ccg_pred = torch.hstack((ccg_pred, torch.argmin(dists, 1)))
+        true_targets = torch.hstack((true_targets, targets))
+        break
+    print(f"Classifier accuracy: {torch.sum(classifier_pred == true_targets) / len(true_targets)}")
+    print(f"CCG accuracy: {torch.sum(ccg_pred == true_targets) / len(true_targets)}")
