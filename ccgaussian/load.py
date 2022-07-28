106339dc44995533aab97f3257173ce1ee7333ba
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-07-28 11:02:02 -0400

    Move Loading Inside Package

---
+++ ccgaussian/load.py
@@ -0,0 +1,18 @@
+import torch
+
+import ccgaussian.model
+
+
+def load_dino_ccg(map_location=torch.device("cpu"), pretrained=True, **kwargs):
+    num_classes = 5
+    model = ccgaussian.model.DinoCCG(num_classes, **kwargs)
+    if pretrained:
+        state_dict = torch.hub.load_state_dict_from_url(
+            "https://github.com/tufts-ai-robotics-group/CCGaussian/releases/download/Model/DinoCCG_0_0_1.pt",  # noqa: E501
+            "runs/DinoCCG_0_0_1",
+            map_location=map_location,
+        )
+        model.load_state_dict(state_dict)
+    else:
+        model = model.to(map_location)
+    return model
--- hubconf.py
+++ hubconf.py
@@ -1,21 +1,10 @@
 import torch
 
-import ccgaussian.model
+import ccgaussian.load
 
 
 dependencies = ["torch"]
 
 
 def dino_ccg(map_location=torch.device("cpu"), pretrained=True, **kwargs):
-    num_classes = 5
-    model = ccgaussian.model.DinoCCG(num_classes, **kwargs)
-    if pretrained:
-        state_dict = torch.hub.load_state_dict_from_url(
-            "https://github.com/tufts-ai-robotics-group/CCGaussian/releases/download/Model/DinoCCG_0_0_1.pt",  # noqa: E501
-            "runs/DinoCCG_0_0_1",
-            map_location=map_location,
-        )
-        model.load_state_dict(state_dict)
-    else:
-        model = model.to(map_location)
-    return model
+    return ccgaussian.load.load_dino_ccg(map_location, pretrained, **kwargs)
