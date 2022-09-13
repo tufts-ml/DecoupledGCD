8e8d2bc88ee26fdce0bd27a84a647dedbfa715b6
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-09-13 11:47:57 -0400

    GCD Model Loading

--- ccgaussian/load.py
+++ ccgaussian/load.py
@@ -8,7 +8,19 @@ def load_dino_ccg(pretrained=True, **kwargs):
     model = ccgaussian.model.DinoCCG(num_classes, **kwargs)
     if pretrained:
         state_dict = torch.hub.load_state_dict_from_url(
-            "https://github.com/tufts-ai-robotics-group/CCGaussian/releases/download/Model/DinoCCG_0_0_1.pt",  # noqa: E501
+            "https://github.com/tufts-ai-robotics-group/CCGaussian/releases/download/NoveltyModel/DinoCCG_0_0_1.pt",  # noqa: E501
+            map_location="cpu",
+        )
+        model.load_state_dict(state_dict)
+    return model
+
+
+def load_ccg_gcd(pretrained=True, **kwargs):
+    num_classes = 5
+    model = ccgaussian.model.DinoCCG(num_classes, **kwargs)
+    if pretrained:
+        state_dict = torch.hub.load_state_dict_from_url(
+            "https://github.com/tufts-ai-robotics-group/CCGaussian/releases/download/GCDModel/CCGGCD_0_0_1.pt",  # noqa: E501
             map_location="cpu",
         )
         model.load_state_dict(state_dict)
--- hubconf.py
+++ hubconf.py
@@ -6,3 +6,7 @@ dependencies = ["torch"]
 
 def dino_ccg(pretrained=True, **kwargs):
     return ccgaussian.load.load_dino_ccg(pretrained, **kwargs)
+
+
+def ccg_gcd(pretrained=True, **kwargs):
+    return ccgaussian.load.load_ccg_gcd(pretrained, **kwargs)
