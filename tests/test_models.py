f52320a9819cb4d05cf0343cd784dab596a1e2eb
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-12-14 11:17:10 -0500

    Remove Variance Test

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,8 +1,6 @@
 import pytest
 import torch
-from torch.optim import SGD
 
-from ccgaussian.loss import NDCCLoss
 from ccgaussian.model import DinoCCG
 
 
@@ -21,27 +19,3 @@ class TestDinoCCG():
         assert norm_embeds.shape == (input_shape[0], model.embed_len)
         assert means.shape == (num_classes, model.embed_len)
         assert sigma2s.shape == (model.embed_len,)
-
-    def test_var_stability(self, model):
-        norm_embeds = torch.zeros((input_shape[0], model.embed_len))
-        active_dims = 4
-        norm_embeds[:, :active_dims] = model.embed_mag / active_dims
-        targets = torch.zeros((input_shape[0],), dtype=int)
-        optimizer = SGD([
-            {"params": [model.sigma, model.deltas], "lr": 1e-4, "momentum": 0},
-            {"params": [model.classifier.weight], "lr": 1e-1, "momentum": .9}])
-        # generous loss threshold that should be easy to reach and stay under
-        loss_thresh = -400
-        passed_loss_thresh = False
-        for _ in range(100):
-            optimizer.zero_grad()
-            means, sigma2s = model.gaussian_params()
-            loss = NDCCLoss.nll_loss(norm_embeds, means, sigma2s, targets)
-            loss.backward()
-            optimizer.step()
-            if passed_loss_thresh and loss > loss_thresh:
-                pytest.fail("Optimization not stable, went under then above threshold")
-            if loss < loss_thresh:
-                passed_loss_thresh = True
-        if passed_loss_thresh is False:
-            pytest.fail("Optimization failed to reach threshold")
