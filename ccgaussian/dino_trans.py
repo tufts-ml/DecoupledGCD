8736111ab702eae7b6f0b18fb79a2ab9420ba796
PatrickFeeney <patrickfeeney1225@gmail.com>
2023-01-13 09:30:19 -0500

    VGG Norm -> DINO ImageNet Norm

--- ccgaussian/dino_trans.py
+++ ccgaussian/dino_trans.py
@@ -44,7 +44,7 @@ class DINONormTrans:
     def __init__(self) -> None:
         self.transform = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
         ])
 
     def __call__(self, img):
