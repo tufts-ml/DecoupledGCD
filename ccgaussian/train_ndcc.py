85b5e51de489b751155d7349eb4a1ed0a74a61ec
PatrickFeeney <patrickfeeney1225@gmail.com>
2023-01-13 10:46:11 -0500

    GCDdatasets Training

--- ccgaussian/train_ndcc.py
+++ ccgaussian/train_ndcc.py
@@ -3,18 +3,26 @@ from pathlib import Path
 
 from sklearn.metrics import roc_auc_score
 import torch
+from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
-from polycraft_nov_data.dataloader import novelcraft_dataloader
+from gcd_data.get_datasets import get_class_splits, get_datasets
 
-from ccgaussian.dino_trans import DINOTestTrans
+from ccgaussian.dino_trans import sim_gcd_train, sim_gcd_test
 from ccgaussian.loss import NDCCLoss, novelty_md
 from ccgaussian.model import DinoCCG
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset", type=str, default="NovelCraft", choices=["NovelCraft"])
+    # dataset and split arguments
+    parser.add_argument(
+        "--dataset_name", type=str, default="cub",
+        choices=["NovelCraft", "cifar10", "cifar100", "imagenet_100", "cub", "scars",
+                 "fgvc_aricraft", "herbarium_19"],
+        help="options: NovelCraft, cifar10, cifar100, imagenet_100, cub, scars, fgvc_aricraft, " +
+             "herbarium_19")
+    parser.add_argument('--prop_train_labels', type=float, default=0.5)
     # model label for logging
     parser.add_argument("--label", type=str, default=None)
     # training hyperparameters
@@ -36,29 +44,51 @@ def get_args():
     parser.add_argument("--w_nll", type=float, default=1e-2,
                         help="Negative log-likelihood weight for embedding network")
     args = parser.parse_args()
-    # add dataset related args
-    if args.dataset == "NovelCraft":
-        args.num_classes = 5
     # prepend runs folder to label if given
     if args.label is not None:
         args.label = "runs/" + args.label
     return args
 
 
+def get_nd_dataloaders(args):
+    """Get novelty detection DataLoaders
+
+    Args:
+        args (Namespace): args containing dataset and prop_train_labels
+
+    Returns:
+        tuple: train_loader: Normal training set
+            valid_loader: Normal and novel validation set
+            test_loader: Normal and novel test set
+            args: args updated with num_labeled_classes and num_unlabeled_classes
+    """
+    train_trans = sim_gcd_train()
+    test_trans = sim_gcd_test()
+    args = get_class_splits(args)
+    dataset_dict = get_datasets(args.dataset_name, train_trans, test_trans, args)[-1]
+    # add number of labeled and unlabeled classes to args
+    args.num_labeled_classes = len(args.train_classes)
+    args.num_unlabeled_classes = len(args.unlabeled_classes)
+    # construct DataLoaders
+    dataloader_kwargs = {
+        "batch_size": args.batch_size,
+        "num_workers": 4,
+    }
+    train_loader = DataLoader(dataset_dict["train_labeled"], **dataloader_kwargs)
+    valid_loader = DataLoader(dataset_dict["test"], **dataloader_kwargs)
+    test_loader = DataLoader(dataset_dict["train_unlabeled"], **dataloader_kwargs)
+    return train_loader, valid_loader, test_loader, args
+
+
 def train_ndcc(args):
     # choose device
     device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
     # init dataloaders
-    valid_nov_loader = None
-    if args.dataset == "NovelCraft":
-        train_loader = novelcraft_dataloader("train", DINOTestTrans(), args.batch_size,
-                                             balance_classes=True)
-        valid_norm_loader = novelcraft_dataloader("valid_norm", DINOTestTrans(), args.batch_size,
-                                                  balance_classes=False)
-        valid_nov_loader = novelcraft_dataloader("valid_novel", DINOTestTrans(), args.batch_size,
-                                                 balance_classes=False)
+    train_loader, valid_loader, test_loader, args = get_nd_dataloaders(args)
+    normal_classes = torch.tensor(list(args.train_classes))
     # init model
-    model = DinoCCG(args.num_classes, args.init_var, args.end_var, args.var_milestone).to(device)
+    model = DinoCCG(
+        args.num_labeled_classes, args.init_var, args.end_var, args.var_milestone).to(device)
     # init optimizer
     optim = torch.optim.SGD([
         {
@@ -75,9 +105,7 @@ def train_ndcc(args):
     ])
     scheduler = torch.optim.lr_scheduler.MultiStepLR(
         optim, milestones=args.lr_milestones, gamma=0.5)
-    phases = ["train", "val_norm"]
-    if valid_nov_loader is not None:
-        phases.append("val_nov")
+    phases = ["train", "valid", "test"]
     # init loss
     loss_func = NDCCLoss(args.w_nll)
     # init tensorboard
@@ -86,89 +114,98 @@ def train_ndcc(args):
     metric_dict = {}
     # model training
     for epoch in range(args.num_epochs):
-        epoch_novel_scores = torch.Tensor([])
-        epoch_novel_labels = torch.Tensor([])
         # update model variance
         model.anneal_var(epoch)
-        # Each epoch has a training and validation phase
+        # Each epoch has a training, validation, and test phase
         for phase in phases:
             if phase == "train":
                 model.train()
                 dataloader = train_loader
-            elif phase == "val_norm":
+            elif phase == "valid":
                 model.eval()
-                dataloader = valid_norm_loader
+                dataloader = valid_loader
             else:
                 model.eval()
-                dataloader = valid_nov_loader
+                dataloader = test_loader
+            # novelty prediction storage for non-training phases
+            novel_scores = torch.Tensor([])
+            novel_labels = torch.Tensor([])
             # vars for tensorboard stats
             cnt = 0
             epoch_loss = 0.
             epoch_acc = 0.
             epoch_nll = 0.
-            sigma2s_mean = 0.
-            for data, targets in dataloader:
+            epoch_sigma2s = 0.
+            for data, targets, uq_idxs in dataloader:
                 # forward and loss
-                data = (data.to(device))
-                targets = (targets.long().to(device))
+                data = data.to(device)
+                targets = targets.long().to(device)
                 optim.zero_grad()
                 with torch.set_grad_enabled(phase == "train"):
                     logits, norm_embeds, means, sigma2s = model(data)
-                    if phase != "val_nov":
+                    if phase == "train":
                         loss = loss_func(logits, norm_embeds, means, sigma2s, targets)
+                    else:
+                        # filter out novel examples from loss in non-training phases
+                        norm_mask = torch.isin(targets, normal_classes)
+                        loss = loss_func(logits[norm_mask], norm_embeds[norm_mask], means, sigma2s,
+                                         targets[norm_mask])
                 # backward and optimize only if in training phase
                 if phase == "train":
                     loss.backward()
                     optim.step()
-                # novelty detection stats in validation phase if there's a validation novel set
-                elif "val_nov" in phases:
-                    novel_scores = novelty_md(norm_embeds, means, sigma2s).detach().cpu()
-                    epoch_novel_scores = torch.hstack([epoch_novel_scores, novel_scores])
-                    epoch_novel_labels = torch.hstack([epoch_novel_labels, torch.Tensor(
-                        [1 if phase == "val_nov" else 0] * len(novel_scores))])
+                # novelty detection stats in non-training phase
+                else:
+                    cur_novel_scores = novelty_md(norm_embeds, means, sigma2s).detach().cpu()
+                    novel_scores = torch.hstack([novel_scores, cur_novel_scores])
+                    novel_labels = torch.hstack([novel_labels, torch.logical_not(norm_mask).int()])
                 # calculate statistics
-                if phase != "val_nov":
-                    _, preds = torch.max(logits, 1)
-                    epoch_loss = (loss.item() * data.size(0) +
-                                  cnt * epoch_loss) / (cnt + data.size(0))
-                    epoch_acc = (torch.sum(preds == targets.data) +
-                                 epoch_acc * cnt).double() / (cnt + data.size(0))
-                    epoch_nll = (NDCCLoss.nll_loss(norm_embeds, means, sigma2s, targets) +
-                                 epoch_nll * cnt) / (cnt + data.size(0))
-                    sigma2s_mean = (torch.mean(sigma2s) * data.size(0) +
-                                    sigma2s_mean * cnt) / (cnt + data.size(0))
+                _, preds = torch.max(logits, 1)
+                epoch_loss = (loss.item() * data.size(0) +
+                              cnt * epoch_loss) / (cnt + data.size(0))
+                epoch_acc = (torch.sum(preds == targets.data) +
+                             epoch_acc * cnt).double() / (cnt + data.size(0))
+                epoch_nll = (NDCCLoss.nll_loss(norm_embeds, means, sigma2s, targets) +
+                             epoch_nll * cnt) / (cnt + data.size(0))
+                epoch_sigma2s = (torch.mean(sigma2s) * data.size(0) +
+                                 epoch_sigma2s * cnt) / (cnt + data.size(0))
                 cnt += data.size(0)
+            # get phase label and update LR scheduler
             if phase == "train":
                 phase_label = "Train"
                 scheduler.step()
-            else:
+            elif phase == "valid":
                 phase_label = "Valid"
+            else:
+                phase_label = "Test"
             # output statistics
-            if phase != "val_nov":
-                writer.add_scalar(f"{phase_label}/Average Loss", epoch_loss, epoch)
-                writer.add_scalar(f"{phase_label}/Average Accuracy", epoch_acc, epoch)
-                writer.add_scalar(f"{phase_label}/Average NLL", epoch_nll, epoch)
-                if epoch == args.num_epochs - 1 and phase == "val_norm":
-                    metric_dict = {
-                        "hparam/val_loss": epoch_loss,
-                        "hparam/val_accuracy": epoch_acc,
-                        "hparam/val_nll": epoch_nll,
-                    }
-        # determine validation AUROC after all phases
-        if "val_nov" in phases:
-            epoch_auroc = roc_auc_score(epoch_novel_labels, epoch_novel_scores)
-            writer.add_scalar(f"{phase_label}/NovDet AUROC", epoch_auroc, epoch)
-            if epoch == args.num_epochs - 1:
-                metric_dict["hparam/val_auroc"] = epoch_auroc
-                # record hparams all at once and after all other writer calls
-                # to avoid issues with Tensorboard updating current file
-                writer.add_hparams({
-                    "lr_e": args.lr_e,
-                    "lr_c": args.lr_c,
-                    "init_var": args.init_var,
-                    "end_var": args.end_var,
-                    "w_nll": args.w_nll,
-                }, metric_dict, run_name=".")
+            writer.add_scalar(f"{phase_label}/Average Loss", epoch_loss, epoch)
+            writer.add_scalar(f"{phase_label}/Average Accuracy", epoch_acc, epoch)
+            writer.add_scalar(f"{phase_label}/Average NLL", epoch_nll, epoch)
+            # only output variance for training since other phases will match it
+            if phase == "train":
+                writer.add_scalar(f"{phase_label}/Average Variance Mean", epoch_sigma2s, epoch)
+            # determine AUROC for non-training phases
+            if phase != "train":
+                auroc = roc_auc_score(novel_labels, novel_scores)
+                writer.add_scalar(f"{phase_label}/NovDet AUROC", auroc, epoch)
+                # record end of training stats
+                if epoch == args.num_epochs - 1:
+                    metric_dict.update({
+                        f"hparam/{phase_label}_loss": epoch_loss,
+                        f"hparam/{phase_label}_accuracy": epoch_acc,
+                        f"hparam/{phase_label}_nll": epoch_nll,
+                        f"hparam/{phase_label}_auroc": auroc,
+                    })
+    # record hparams all at once and after all other writer calls
+    # to avoid issues with Tensorboard changing output file
+    writer.add_hparams({
+        "lr_e": args.lr_e,
+        "lr_c": args.lr_c,
+        "init_var": args.init_var,
+        "end_var": args.end_var,
+        "w_nll": args.w_nll,
+    }, metric_dict, run_name=".")
     torch.save(model.state_dict(), Path(writer.get_logdir()) / f"{args.num_epochs}.pt")
 
 
