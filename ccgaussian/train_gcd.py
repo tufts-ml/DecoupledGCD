01839721281fc0d207946ad3c2e73d08dea7455f
PatrickFeeney <patrickfeeney1225@gmail.com>
2023-04-04 09:57:22 -0400

    norm_mask Bool not Int

--- ccgaussian/train_gcd.py
+++ ccgaussian/train_gcd.py
@@ -182,7 +182,6 @@ def train_gcd(args):
                 norm_mask = norm_mask.to(device)
                 # create normal soft targets
                 num_samples = data.shape[0]
-                # TODO a bunch of soft_targets staying as zeros
                 soft_targets = torch.zeros((num_samples, num_classes)).to(device)
                 soft_targets[torch.arange(num_samples).to(device)[norm_mask],
                              targets[norm_mask]] = 1
