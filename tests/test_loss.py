f8977c3b69b7d9504a939e3a042b3ef8ad045d5a
PatrickFeeney <patrickfeeney1225@gmail.com>
2023-04-27 15:27:00 -0400

    Fix Loss Test

--- tests/test_loss.py
+++ tests/test_loss.py
@@ -34,7 +34,7 @@ class TestNDCCLoss():
 def test_empty_soft():
     loss = ccgaussian.loss.GMMFixedLoss(w_nll=.025, w_unlab=0, pseudo_thresh=0)
     logits, embeds, means, sigma2s, targets, norm_mask = dummy_data(0)
-    assert loss(logits, embeds, means, sigma2s, means, targets, norm_mask) == 0
+    assert loss(logits, embeds, means, sigma2s, targets, norm_mask) == 0
 
 
 def test_soft_consistent():
@@ -45,5 +45,5 @@ def test_soft_consistent():
     soft_targets = torch.zeros((num_samples, num_classes))
     soft_targets[torch.arange(num_samples), targets] = 1
     loss_val = loss(logits, embeds, means, sigma2s, targets)
-    soft_loss_val = soft_loss(logits, embeds, means, sigma2s, means, soft_targets, norm_mask)
+    soft_loss_val = soft_loss(logits, embeds, means, sigma2s, soft_targets, norm_mask)
     assert torch.allclose(loss_val, soft_loss_val)
