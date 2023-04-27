36dd2f34a3fee7fb665ec7adc4fc9c953ee8bb43
PatrickFeeney <patrickfeeney1225@gmail.com>
2023-04-27 13:51:23 -0400

    MD Plots

--- ccgaussian/test/plot.py
+++ ccgaussian/test/plot.py
@@ -28,3 +28,29 @@ def plot_gcd_ci(all, normal, novel):
     ax.set_xticklabels(["All", "Normal", "Novel"])
     ax.set_ylabel("Accuracy")
     return fig
+
+
+def plot_md_hist(embeds, targets, means, sigma2s):
+    fig, ax = plt.subplots()
+    num_embeds = embeds.shape[0]
+    md = ((embeds - means[targets])**2 / sigma2s).sum(axis=1)**(.5)
+    ax.hist(md, weights=np.array([1 / num_embeds] * num_embeds))
+    ax.set_xlabel("Mahalanobis Distance")
+    ax.set_ylabel("Distance Frequency")
+    return fig
+
+
+def plot_md_scatter(embeds, targets, means, sigma2s):
+    fig, ax = plt.subplots()
+    num_embeds = embeds.shape[0]
+    # get Mahalanobis distance and cosine similarity for points
+    md = ((embeds - means[targets])**2 / sigma2s).sum(axis=1)**(.5)
+    cos_sim = np.empty((num_embeds,))
+    for target in np.unique(targets):
+        tar_mask = targets == target
+        cos_sim[tar_mask] = embeds[tar_mask] @ means[target] / \
+            (np.linalg.norm(embeds[tar_mask], axis=1) * np.linalg.norm(means[target]))
+    ax.scatter(md, cos_sim, alpha=.1)
+    ax.set_xlabel("Mahalanobis Distance")
+    ax.set_ylabel("Cosine Similarity")
+    return fig
