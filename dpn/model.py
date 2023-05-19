d12acc634b8460f2bec3ac429425943dfa710195
PatrickFeeney <patrickfeeney1225@gmail.com>
2023-05-19 09:18:37 -0400

    Fix set_protos Device

--- dpn/model.py
+++ dpn/model.py
@@ -8,12 +8,13 @@ class DPN(nn.Module):
         self.num_classes = num_classes
         self.num_labeled_classes = num_labeled_classes
         self.l_moment = l_moment
-        self.set_protos(l_proto, u_proto)
         # pretrained DINO backbone
         self.dino = torch.hub.load('facebookresearch/dino:main', 'dino_vitb16')
         self.embed_len = self.dino.norm.normalized_shape[0]
         # linear classification head for labeled classes only
         self.classifier = nn.Linear(self.embed_len, num_labeled_classes)
+        # initialize prototypes after setting up other parameters
+        self.set_protos(l_proto, u_proto)
 
     def forward(self, x):
         # DINO embeddings
@@ -24,8 +25,9 @@ class DPN(nn.Module):
         return logits, embeds
 
     def set_protos(self, l_proto, u_proto):
-        self.l_proto = nn.Parameter(l_proto.detach().to(self.device), requires_grad=False)
-        self.u_proto = nn.Parameter(u_proto.detach().to(self.device), requires_grad=False)
+        device = next(self.parameters()).device
+        self.l_proto = nn.Parameter(l_proto.detach().to(device), requires_grad=False)
+        self.u_proto = nn.Parameter(u_proto.detach().to(device), requires_grad=False)
 
     def update_l_proto(self, label_embeds, label_targets):
         new_l_proto = torch.zeros_like(self.l_proto)
